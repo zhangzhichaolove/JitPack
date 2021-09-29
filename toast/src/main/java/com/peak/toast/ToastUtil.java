b1402441e9e5eae2bf5a6feb14741b5547d96f1c
peakchao <zhangzhichaolove@vip.qq.com>
2021-09-29 17:09:31 +0800

    add toast utils.

---
+++ toast/src/main/java/com/peak/toast/ToastUtil.java
@@ -0,0 +1,12 @@
+package com.peak.toast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    public static void show(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+}
