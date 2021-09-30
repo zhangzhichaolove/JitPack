44a8dfca9813d09c65f39d10e78a113de93c8fdb
peakchao <zhangzhichaolove@vip.qq.com>
2021-09-30 09:26:04 +0800

    test lib.

--- app/src/main/java/com/peak/jitpack/MainActivity.java
+++ app/src/main/java/com/peak/jitpack/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.peak.toast.ToastUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToastUtil.show(this,"提示!");
     }
 }
